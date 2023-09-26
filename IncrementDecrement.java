446a3160043b81ddf3a3239dca2d24b3d480d4d9
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-09-26 17:08:38 +0530

    Add files via upload

---
+++ IncrementDecrement.java
@@ -0,0 +1,28 @@
+package unary.operaters;
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=20;
+		b=++a;
+		System.out.println("a="+a+",b="+b);
+		
+		a=30;
+		b=a++;
+		System.out.println("a="+a+",b="+b);
+		
+		a=12;
+		b=--a;
+		System.out.println("a="+a+",b="+b);
+		
+		a=10;
+		b=a--;
+		System.out.println("a="+a+",b="+b);
+		
+		
+		
+		
+	}
+
+}
