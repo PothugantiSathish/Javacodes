af435d00b93a2907fd3c6aab3c8e048fcac9a79c
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-10-07 11:01:10 +0530

    Add files via upload

---
+++ Replace.java
@@ -0,0 +1,15 @@
+package com.uno.carrer;
+
+public class Replace {
+
+	public static void main(String[] args) {
+		String data = "welcome to india.  i Love My india."
+				+ "  india is very Beautiful culture  country."
+				+ "I am Living In hyderabad";
+		data=data.replace("india","USA");
+		System.out.println(data);
+		
+		
+	}
+
+}
