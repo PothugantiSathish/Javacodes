48cc8058be6f791fe52a1c1416b3a15e267fdc07
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-10-10 08:15:04 +0530

    Add files via upload

---
+++ Grades.java
@@ -0,0 +1,19 @@
+package selection;
+
+public class Grades {
+
+	public static void main(String[] args) {
+		int a=72;
+		if(a<60) {
+			System.out.println("F");
+		}else if(a>=60&&a<=74) {
+			System.out.println("C");
+		}else if(a>=75&&a<=89) {
+			System.out.println("B");
+		}else {
+			System.out.println("A");
+		}
+
+	}
+
+}
