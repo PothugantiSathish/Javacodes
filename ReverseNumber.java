7a3ecde28131c4eb8a2d9c18d5576926546caee5
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-10-10 08:17:11 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,15 @@
+package selection;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		int n=12678,r,reverse=0;
+		while(n>0) {
+			r=n%10;
+			reverse=reverse*10+r;
+			n=n/10;
+		}
+		System.out.println("reverse="+reverse);
+	}
+
+}
