124d5fd769975fe2f45158819bdfe4760a29c7cc
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-10-29 12:44:33 +0530

    Add files via upload

---
+++ functiondata.js
@@ -0,0 +1,17 @@
+function design(data){
+    if(data=='*')
+    console.log("********")
+    else if(data=='$')
+    console.log("$$$$$$$$$")
+    else if(data=='.')
+    console.log(".......")
+    else if(data=='-')
+    console.log("-------")
+}
+
+design('*')
+console.log("Hii everyone")
+design('$')
+console.log("Good Morning")
+design('-')
+console.log("My name is")
