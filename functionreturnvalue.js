c6cbba3e44338dd79594197743e630686c123844
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-10-29 12:46:04 +0530

    Add files via upload

---
+++ functionreturnvalue.js
@@ -0,0 +1,10 @@
+function usDollarToRuppes(dollars)
+{
+    return dollars * 87.45;
+   
+}
+
+let noDollars = 200
+let rupees = usDollarToRuppes(noDollars);
+
+console.log("Indian rupees  Rs." +rupees+"    for US Dollars   U$"+noDollars)
