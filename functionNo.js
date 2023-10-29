a9c3db0604ced457ddb4cbc8a8084f36865b3135
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-10-29 12:45:21 +0530

    Add files via upload

---
+++ functionNo.js
@@ -0,0 +1,10 @@
+function usDollar()
+{
+    return 87.45;
+   
+}
+
+let dollars =100
+let rupees = usDollar() * dollars;
+
+console.log("Indian rupees  Rs." +rupees+"    for US Dollars   U$"+dollars)
