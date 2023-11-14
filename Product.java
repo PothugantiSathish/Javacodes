e5573c3de9109c4322004e2aba0a8649d4a6161a
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-14 19:52:52 +0530

    Add files via upload

--- Product.java
+++ Product.java
@@ -25,6 +25,12 @@ class Product {
 		this.ProductName = productName;
 	}
 	public static void main(String[] args) {
+		Product p1=new Product(101,"Chocalates",1000,"Dairy Milk");
+		Product p2=new Product(102,"Biscuits",1000);
+		Product p3=new Product(103,"Kinder joy");
+		System.out.println(p1.ProductId+" "+p1.ProductName+" "+p1.ProductPrice+" "+p1.ProductBrand);
+		System.out.println(p2.ProductId+" "+p1.ProductName+" "+p1.ProductPrice);
+		System.out.println(p3.ProductId+" "+p1.ProductName);
 		
 
 	}
